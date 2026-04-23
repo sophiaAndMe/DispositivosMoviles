6c7b0ef078b84c13995f9323b27264febac7d40a
JhonnyNinabanda <jeninabandap@uce.edu.ec>
2026-04-22 19:04:33 -0500

    eventos y inflar xml usando binding

--- app/src/main/java/com/example/myapplication/MobinLogin.kt
+++ app/src/main/java/com/example/myapplication/MobinLogin.kt
@@ -1,20 +1,66 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMobinLoginBinding
 
 class MobinLogin : AppCompatActivity() {
+
+
+    // Son lazy -> solo se crean cuando se necesita
+    // tambien vemos ambito (scope), estan esta a nivel de clase
+    lateinit var binding : ActivityMobinLoginBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_mobin_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // ya tiene todos los elementos que tenga el Id
+        binding = ActivityMobinLoginBinding.inflate(layoutInflater)
+
+
+        setContentView(binding.root)
+
+
+
+        // set es actualizar
+        binding.buttonLogin.setOnClickListener {
+
+            var msg = ""
+
+            if( binding.txtUsername.text.toString() == "admin" && binding.txtPassword.text.toString() == "root"){
+
+                msg = "Usuario autenticado correctamente"
+
+                Toast.makeText(
+                    this,
+                    msg,
+                    Toast.LENGTH_LONG).show()
+            }else{
+                msg = "Usuario incorrecto"
+
+                Toast.makeText(
+                    this,
+                    msg,
+                    Toast.LENGTH_LONG).show()
+            }
+
+
+
+
         }
+
+
+
+
+
     }
+
 }
